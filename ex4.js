f128501e4d3a164b0b2ae5f88620114b8e96256d
ssuthon <suthon@gmail.com>
2015-06-08 07:11:32 +0000

    ex4,5

---
+++ ex4.js
@@ -0,0 +1,6 @@
+var findMax = function(data){
+    
+}
+
+var data = [2, 3, 1, 7];
+console.log(findMax(data));
---
+++ ex5.js
@@ -0,0 +1,6 @@
+var findAvg = function(data){
+    
+}
+
+var data = [2, 3, 1, 7];
+console.log(findAvg(data));
