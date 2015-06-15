f3ffa71d96b2d42d05b80be89dd27ce0e8203d56
ssuthon <suthon@gmail.com>
2015-06-15 07:24:38 +0000

    ex7

---
+++ ex7.js
@@ -0,0 +1,9 @@
+var students = [
+	{name: 'John', score: 65},
+	{name: 'Jane', score: 60},
+	{name: 'Kent', score: 55},
+	{name: 'Kate', score: 78}
+];
+
+console.log(calTotalScore(students));
+console.log(findTopStudent(students).name);
